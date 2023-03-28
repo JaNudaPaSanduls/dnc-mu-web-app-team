a7ca826d5610ed29e166d913f7b4bbd59d2d2f44
J a N u d a <januda.me@gmail.com>
2023-03-29 05:29:45 +0530

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,55 @@
+import dnc from "./dnc-credit.png";
+import app_des from "./app.png";
+import dnc_mu from "./dnc-mu-credit.png";
+import tech_logo from "./tech_logo.png";
+import dnc_logo from "./dnc_logo.svg";
+import logo from "./logo.svg";
+import web_logo from "./web_logo.svg";
+import quotes from "./quotes.svg";
+import robot from "./robot.png";
+import filter from "./Filter.svg";
+import shield from "./shield.svg";
+import discover from "./discovery.svg";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import google from "./google.svg";
+import github from "./github.svg";
+import apple from "./apple.svg";
+import arrowUp from "./arrow-up.svg";
+import dnc_min_logo from "./dnc-min-logo.svg";
+import facebook from "./facebook.svg";
+import instagram from "./instagram.svg";
+import linkedin from "./linkedin.svg";
+import twitter from "./twitter.svg";
+import chalitha from "./chalitha.svg";
+import januda from "./januda.png";
+import tiran from "./tiran.svg";
+
+export {
+  dnc,
+  app_des,
+  dnc_mu,
+  tech_logo,
+  dnc_logo,
+  logo,
+  web_logo,
+  quotes,
+  robot,
+  filter,
+  shield,
+  discover,
+  menu,
+  close,
+  google,
+  apple,
+  github,
+  arrowUp,
+  dnc_min_logo,
+  facebook,
+  instagram,
+  linkedin,
+  twitter,
+  chalitha,
+  januda,
+  tiran
+};
